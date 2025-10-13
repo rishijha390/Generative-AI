c81ccff7a36664bb7514c227c609cf168ea55233
rishijha390 <rishijha390@gmail.com>
2025-10-13 23:45:33 +0530

    added files to the prompts folder

--- prompts/few.py
+++ prompts/few.py
@@ -5,26 +5,31 @@ from openai import OpenAI
 load_dotenv()
 
 client = OpenAI(
-    api_key = "##",
-    base_url="##"
+    api_key = "AIzaSyBL1JEYgWn2QBvoy5M1-4ToUx9taQ0JRn0",
+    base_url="https://generativelanguage.googleapis.com/v1beta/"
 )
 # directly giving instruction to the model is termed as zero short prompting
 SYSTEM_PROMPT = """
 You should only and only answer the coding related questions.Do not answer anything else your name is alexa if user asked something other then coding just say sorry
 
+Output Format:
+{{
+"code": "string" or null,
+"isCodingQuestion": boolean
+}}
 Examples:
 Q.can you explain the a+b whole square?
-A.Sorry,I can only help with the codding related questions
+A.{{ "code": null, "isCodingQuestion": false}}
 
         Q. Hey ,Write a code in python for adding two numbers.
-        A. def add(a,b):
-        return a + b
+        A. {{ "code": "def add(a,b):
+        return a + b", "isCodingQuestion": false}}
 """
 response = client.chat.completions.create(
     model="gemini-2.5-flash",
     messages=[
         {"role": "system", "content": SYSTEM_PROMPT},
-        { "role": "user", "content" : "can you explain me a + b whole square" }
+        { "role": "user", "content" : "Hey,write a code to add n numbers in javascript" }
     ]
 )
 
