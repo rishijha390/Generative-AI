5d13249cf0af9df2b8a342fd138caa2caf670bc5
rishijha390 <rishijha390@gmail.com>
2025-10-13 23:10:19 +0530

    added files to the prompts folder

---
+++ prompts/zero.py
@@ -0,0 +1,22 @@
+from dotenv import load_dotenv
+from openai import OpenAI
+
+
+load_dotenv()
+
+client = OpenAI(
+    api_key = "AIzaSyBL1JEYgWn2QBvoy5M1-4ToUx9taQ0JRn0",
+    base_url="https://generativelanguage.googleapis.com/v1beta/"
+)
+# directly giving instruction to the model is termed as zero short prompting
+SYSTEM_PROMPT = "You should only and only answer the coding related questions.Do not answer anything else your name is alexa if user asked something other then coding just say sorry"
+response = client.chat.completions.create(
+    model="gemini-2.5-flash",
+    messages=[
+        {"role": "system", "content": SYSTEM_PROMPT},
+        { "role": "user", "content" : "can you tell me a joke" }
+    ]
+)
+
+print(response.choices[0].message.content)
+# the model is given direct question  or task without prior examples
